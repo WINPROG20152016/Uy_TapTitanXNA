39656522e05174e60d4f19414755b0621e52eec7
unknown <istudent@CL1-0.iacademy.loc>
2015-08-15 09:06:59 +0800

    Remove line

--- TapTitanXNA/TapTitanXNA/TapTitanXNA/Level.cs
+++ TapTitanXNA/TapTitanXNA/TapTitanXNA/Level.cs
@@ -12,7 +12,6 @@ namespace TapTitanXNA
     public class Level
     {
         // ADDED
-        // added comment
 
         public static int windowWidth = 400;
         public static int windowHeight = 500;
